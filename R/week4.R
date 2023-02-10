144647553bbe89f76ea822d8dd39d587de3127ad
“Chulin <chen6496@umn.edu>
2023-02-10 16:22:56 -0600

    Simplified codes

--- R/week4.R
+++ R/week4.R
@@ -8,10 +8,10 @@ glimpse(import_tbl)
 wide_tbl <- import_tbl%>% separate(qs, into = c("q1", "q2","q3","q4","q5"),sep = " - ")
 sapply(wide_tbl[,5:9],as.integer)
 wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
-wide_tbl <- cbind(wide_tbl[,1:4],sapply(wide_tbl[,5:9], function(x=wide_tbl[,5:9]) sub(pattern = "0", x, replacement= "NA")))
+wide_tbl[wide_tbl == 0] <- NA
 
 
 
 
-wide_tbl <-wide_tbl[wide_tbl$q2 != "NA",]
+wide_tbl <-wide_tbl[!is.na(wide_tbl$q2),]
 long_tbl <- wide_tbl%>% pivot_longer(col= c("q1", "q2","q3","q4","q5"))
