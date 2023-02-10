ac543486f3ce76e56db8b39dfcc6348fe3c79079
“Chulin <chen6496@umn.edu>
2023-02-10 14:24:42 -0600

    Convert missing values

--- R/week4.R
+++ R/week4.R
@@ -4,3 +4,8 @@ library(tidyverse)
 
 # Data Import
 import_tbl <- read_delim("../data/week4.dat",delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+glimpse(import_tbl)
+wide_tbl <- import_tbl%>% separate(qs, into = c("q1", "q2","q3","q4","q5"),sep = " - ")
+sapply(wide_tbl[,5:9],as.integer)
+wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
+wide_tbl <- cbind(wide_tbl[,1:4],sapply(wide_tbl[,5:9], function(x=wide_tbl[,5:9]) sub(pattern = "0", x, replacement= "NA")))
